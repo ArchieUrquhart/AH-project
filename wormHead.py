449887d8baaacd0c9b08ee021d392c00a462f65d
ArchieUrquhart <128182020+ArchieUrquhart@users.noreply.github.com>
2025-01-31 12:43:40 +0000

    Update wormHead.py

--- wormHead.py
+++ wormHead.py
@@ -1,15 +1,17 @@
 from wormBody import Node
 
 def move_player(head):
+    #iterate through the list from the begining
     segment = head
-
     while segment.nextNode() is not None:
+        # set the position of each node to that of the previous node
         x = segment.getPos()[0]
         y = segment.getPos()[1]
 
         segment = segment.nextNode()
         segment.setPos(x,y)
-
+        
+"""
 head = Node(0,0)
 node = head
 for i in range(10):
@@ -20,3 +22,4 @@ node = head
 for i in range(10):
     node.append()
     node = node.nextNode()
+"""
