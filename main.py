d3a0777df9814d01d469d10aee77e3116b611f08
ArchieUrquhart <128182020+ArchieUrquhart@users.noreply.github.com>
2025-01-31 12:37:15 +0000

    Update main.py
    comment lines

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ gridX, gridY = 13,13
 grid = [[0]*gridX for i in range(gridY)]
 
 def place_apple():
+    #pick random X,Y coordinate
     appleX = randint(0,len(grid) - 1)
     appleY = randint(0,len(grid) - 1)
 
+    #keep cahnging chosen position until a valid spae is chosen
     while grid[appleX][appleY] != 0:
         appleX = randint(0,len(grid) - 1)
         appleY = randint(0,len(grid[0]) - 1)
