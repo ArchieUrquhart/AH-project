74a84fee2cdc9175b9a96a1a31e0f8fc9ad8a318
ArchieUrquhart <128182020+ArchieUrquhart@users.noreply.github.com>
2025-02-04 15:58:41 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from wormHead import *
+from UI import *
+from highscoretable import *
+
+import pygame as py
 from random import randint
 
 #initialise the board height and width
@@ -6,11 +10,10 @@ gridWidth  = 13
 gridHeight = 13
 
 
-
 def place_apple():
     #pick random X,Y coordinate
-    appleX = randint(0,len(grid) - 1)
-    appleY = randint(0,len(grid[0]) - 1)
+    appleX = randint(0,gridWidth - 1)
+    appleY = randint(0,gridHeight - 1)
 
     #keep cahnging chosen position until a valid spae is chosen
     while grid[appleX][appleY] != 0:
@@ -20,19 +23,36 @@ def place_apple():
     return [appleX, appleY]
 
 
+
 def display_grid(head, appleX, appleY):
-    pass
+    grid = [[0]*gridWidth for i in range(gridHeight)]
+
+    segment = head
+    
+    while segment.nextNode() not None:
+	    x = segment.getPos()[0]
+	    y = segment.getPos()[1]
+
+	    grid[x][y] = 1
+
+	    segment = segment.nextNode()
+
+    
+    grid[appleX][appleY] = 2
+
+    for i in range(0, gridWidth-1):
+    	for j in range(0, gridHeight-1):
+    		colour = GET TYPE @ grid[i][j]
+    		DRAW SQUARE TO SCREEN
+            
+    
+    WRITE SCORE TO SCREEN
+
+
+
     
 
 def gameLoop():
-    #initialise the grid 
-    grid = [[0]*gridWidth for i in range(gridHeight)]
-    
-    # initialsie the player
-    head = Node(round(gridWidth /2) , round(gridHeight /2)
-    head.append()
-    head.nextNode.setPos(gridWidth //2, gridHeight //2 + 1)
-
     #get key inputs 
 
     #move player 
@@ -41,7 +61,15 @@ def gameLoop():
 
     #draw grid    
 
+
+
 def main():
-    pass
+    #initialise the grid 
+    grid = [[0]*gridWidth for i in range(gridHeight)]
+    
+    # initialsie the player
+    head = Node(round(gridWidth /2) , round(gridHeight /2)
+    head.append()
+    head.nextNode.setPos(gridWidth //2, gridHeight //2 + 1)
 
             
