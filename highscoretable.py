f4d809c66d9431bebee61cf6d64348ae8d87cd77
ArchieUrquhart <128182020+ArchieUrquhart@users.noreply.github.com>
2025-02-05 11:41:03 +0000

    Update highscoretable.py

--- highscoretable.py
+++ highscoretable.py
@@ -1,29 +1,62 @@
+import mysql.connector
 #connect to database
+try:
+    cnx = mysql.connector.connect(
+        host="127.0.0.1",
+        database="archie_u ah project",
+        user="pupil",
+        password="pupilpassword"
+    )
 
-#run insert
+except mysql.connector.Error as err:
+    #show error message
+    print("Error with connection: {}".format(err))
+    
+else:
+    #run insert
+    def addGame(game_username, game_points):
+        cursor = cnx.cursor()
+        query = ("INSERT INTO gamedetails_testing(username, score) VALUES (%s,%i")
+        cursor.execute(query,(game_username, game_points))
+        cnx.commit()
+        
+    #get highscore data from database
+    def getTable():
+        #run select
+        cursor = cnx.cursor()
+        query = ("SELECT username, MAX(score) as 'highscore', count(*) as 'games_played' FROM gamedetails_testing GROUP BY username")
+        cursor.execute(query)
+        table_entries = cursor.fethchall()
 
-#run select
+        #init highscore table
+        HighScores = [[[""],[0],[0] for i in range(cursor.rowcount())] 
+        #read select into 2d array
+        for i, (username, highscore, games_played) in enumerate(cursor):
+            HighScores[i][0] = username
+            HighScores[i][1] = highscore
+            HighScores[i][2] = games_played
 
-#read select into 2d array
-
-#sort 2d array
-def sort_table(table):
-    #the column in the table to be sorted by 
-    feild = 1
-
-    #interate through each row in the array
-    for i in range(1,len(table)):
-        #pick the next element in the array
-        temp = arr2d[i]
-        pos = i
-        #move backwards through the array until the correct position is found 
-        while pos > 0 and get_addr(table[pos-1][feild]) > get_addr(temp[field]):
-            #move rows to make space for current row
-            table[pos] = table[pos-1]
-            pos -= 1
-
-        #insert row into correct position
-        table[pos] = temp
-
-    #return sorted 
-    return table
+        return HighScores
+            
+    
+    #sort 2d array
+    def sort_table(table):
+        #the column in the table to be sorted by 
+        feild = 1
+    
+        #interate through each row in the array
+        for i in range(1,len(table)):
+            #pick the next element in the array
+            temp = arr2d[i]
+            pos = i
+            #move backwards through the array until the correct position is found 
+            while pos > 0 and get_addr(table[pos-1][feild]) > get_addr(temp[field]):
+                #move rows to make space for current row
+                table[pos] = table[pos-1]
+                pos -= 1
+    
+            #insert row into correct position
+            table[pos] = temp
+    
+        #return sorted 
+        return table
