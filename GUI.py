176bda4bd86b17dca6e1c5c2657d10ffd1dbe2c9
ArchieUrquhart <128182020+ArchieUrquhart@users.noreply.github.com>
2025-02-11 18:47:31 +0000

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -11,3 +11,10 @@ def get_name():
 	pass
 
 
+def draw_square(x, y, square_size, colour):
+    #positions are changed to account size of each square and the score
+    x_pos = x*square_size
+    y_pos = y*square_size + 100
+    pg.draw.rect(window, colour, (x_pos, y_pos, square_size, square_size))
+
+
