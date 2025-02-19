2fea312228d72c108ba9a8da4af3bf704d73764d
ArchieUrquhart <128182020+ArchieUrquhart@users.noreply.github.com>
2025-02-19 13:05:37 +0000

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,12 +1,40 @@
 import re
-import pygame
+from tkinter import *
 
+
+#function to validate username
 def valid_name(username):
-	if len(username) > 20 or re.search(["^\w"]):
-		return False
-	else:
-		return True
+    #check the length of username and regex to check if there any special characters
+    if len(username) > 20 or len(username) < 4 or re.search("\W",username):
+        return False
+    else:
+        return True
 
-def get_name():
-	pass
+def Take_input(root, inputtxt, label):
+    input = inputtxt.get("1.0", "end-1c")
+    if valid_name(input):
+        root.destroy()
+        username = input
+    else:
+        label.configure(text="Please re-enter your username")
 
+
+def get_username():
+    username = ""
+
+    root = Tk()
+    root.title("Worm")
+
+    label = Label(text="Please enter your username")
+    inputtxt = Text(root, height=1,width=20,padx=20,pady=10)
+    button = Button(root, height=2,width=8,text="Play",command=lambda: Take_input(root,inputtxt,label))
+
+    label.grid(row=0,column=0)
+    inputtxt.grid(row=1, column=0)
+    button.grid(row=1, column=1)
+
+
+
+    mainloop()
+
+    return username
