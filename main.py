c6c1fe55f5f9a2a2f1665fc63ac1d2de26c8a02a
ArchieUrquhart <128182020+ArchieUrquhart@users.noreply.github.com>
2025-02-20 09:57:19 +0000

    Update main.py

--- main.py
+++ main.py
@@ -205,3 +205,12 @@ def game_loop():
 
 
 print(game_loop())
+
+closed = False
+while not closed:
+    #read database into 2d array
+    #display highscore table
+    #get inputs
+    #run game and get the score and username of the player
+    #INSET into table
+    pass
