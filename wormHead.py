d65f871040aadb2e1a16ccfd48d517def6761e36
ArchieUrquhart <128182020+ArchieUrquhart@users.noreply.github.com>
2025-02-25 13:00:08 +0000

    Update wormHead.py

--- wormHead.py
+++ wormHead.py
@@ -1,35 +1,35 @@
 from wormBody import Node
 
-#FR 3.3
+
+# FR 3.3
 def move_player(head):
-    #iterate through the list from the begining
+    # iterate through the list from the begining
     segment = head
     x = segment.getPos()[0]
     y = segment.getPos()[1]
 
     while segment.nextNode() is not None:
-        # set the position of each node to that of the previous node    
+        # set the position of each node to that of the previous node
         segment = segment.nextNode()
-        
+
         newx = segment.getPos()[0]
         newy = segment.getPos()[1]
 
-        segment.setPos(x,y)
+        segment.setPos(x, y)
 
         x = newx
         y = newy
 
 
-
 # detects if worm head is on square with apple FR 5.1 & 4.1
-def detect_eat(head,appleX,appleY):
+def detect_eat(head, appleX, appleY):
     targNode = head
-    #check if head position = apple position
+    # check if head position = apple position
     if head.getPos()[0] == appleX and head.getPos()[1] == appleY:
-        #find last node in list
+        # find last node in list
         while targNode.nextNode() is not None:
             targNode = targNode.nextNode()
-        #add a new node to end of list
+        # add a new node to end of list
         targNode.append()
 
         return True
@@ -38,7 +38,7 @@ def detect_eat(head,appleX,appleY):
 
 
 # checks if player has met loss criteria FR 6.1/6,2
-def check_loss(head):
+def check_loss(head,grid):
     targNode = head.nextNode()
 
     #get position of head
@@ -46,11 +46,11 @@ def check_loss(head):
     headY = head.getPos()[1]
 
     #check if head is out of bounds of grid - FR 6.1
-    if headX< 0 or headX>= gridWidth or headY<0 or headY>=gridHeight:
+    if headX< 0 or headX>= len(grid) or headY<0 or headY>=len(grid[0]):
         return True
     else:
         #Check if worm has hit the body - FR 6.2
-        #loop through whole worm 
+        #loop through whole worm
         while targNode.nextNode() is not None:
             targNode = targNode.nextNode()
             #check if segment intersects with head
