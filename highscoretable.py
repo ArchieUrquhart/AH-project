44dd2172ba2a69b50e5552b82dfbee43aafe0932
ArchieUrquhart <128182020+ArchieUrquhart@users.noreply.github.com>
2025-02-26 13:00:43 +0000

    Update highscoretable.py

--- highscoretable.py
+++ highscoretable.py
@@ -50,21 +50,18 @@ else:
 
     # sort 2d array - FR 1.4
     def sort_table(table):
-        # the column in the table to be sorted by(score)
-        field = 1
 
         # interate through each row in the array
         for i in range(1, len(table)):
             # pick the next element in the array
             temp = table[i]
             pos = i
-            # move backwards through the array until the correct position is found
-            while pos > 0 and table[pos - 1][field] < temp[field]:
+            # move backwards through the array until the correct position is found by comparing highscores
+            while pos > 0 and table[pos - 1][1] < temp[1]:
                 # move rows to make space for current row
                 table[pos] = table[pos - 1]
                 pos -= 1
 
             # insert row into correct position
             table[pos] = temp
-
 
