96446c61033ca64941a548f0b11b421d07dee6c0
ArchieUrquhart <128182020+ArchieUrquhart@users.noreply.github.com>
2025-02-26 13:00:25 +0000

    Update UI.py

--- UI.py
+++ UI.py
@@ -5,12 +5,12 @@ from tkinter import *
 #function to validate username
 def valid_name(username):
     #check the length of username and regex to check if there any special characters
-    if len(username) > 20 or len(username) < 4 or re.search("\W",username):
+    if len(username) > 15 or len(username) < 4 or re.search("\W",username):
         return False
     else:
         return True
 
-def Take_input(root, inputtxt, label):
+def get_input(root, inputtxt, label):
     global username
     input = inputtxt.get("1.0", "end-1c")
     if valid_name(input):
@@ -27,20 +27,18 @@ def get_username():
     root = Tk()
     root.title("Worm")
 
-    label = Label(text="Please enter your username")
+    prompt = Label(text="Please enter your username")
     inputtxt = Text(root, height=1,width=20,padx=20,pady=10)
-    button = Button(root, height=2,width=8,text="Play",command=lambda: Take_input(root,inputtxt,label))
+    button = Button(root, height=2,width=8,text="Play",command=lambda: get_input(root,inputtxt,prompt))
 
-    label.grid(row=0,column=0)
+    prompt.grid(row=0,column=0)
     inputtxt.grid(row=1, column=0)
     button.grid(row=1, column=1)
-
 
 
     mainloop()
 
     return username
-
 
     return username
 
