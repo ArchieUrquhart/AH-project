336e63da24d3ec6ec4ad825eef44f0d1da74aa66
ArchieUrquhart <128182020+ArchieUrquhart@users.noreply.github.com>
2025-03-04 13:07:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -19,17 +19,17 @@ gridWidth, gridHeight = 11, 11
 
 
 
-# draw the game grid - FR 1.5
+#draw the game grid - FR 1.5
 def draw_table(HighScores):
     #fill background colour
     window.fill((0,0,0))
 
-    # write 'LEADERBOARD' to the screen
+    #write 'LEADERBOARD' to the screen
     font = pg.font.Font(None, 100)
     header = font.render("LEADERBOARD", True,(255,255,255))
     window.blit(header,(70,30))
 
-    # write field headings to screen
+    #write field headings to screen
     font = pg.font.Font(None, 50)
     USERNAME = font.render("Username", True, (200, 40, 100))
     HIGHSCORE = font.render("Highscore", True, (90, 150, 240))
@@ -44,11 +44,11 @@ def draw_table(HighScores):
     font = pg.font.Font(None, size)
 
 
-    # start from best player
+    #start from best player
     counter =0
-    # display top 10 players or the number of players if fewer than 10 players have played
+    #display top 10 players or the number of players if fewer than 10 players have played
     while counter < 10 and counter < len(HighScores):
-        # display the current players username score and played games to screen
+        #display the current players username score and played games to screen
         username = font.render("{}".format(HighScores[counter][0]), True, (200, 40, 100))
         highscore = font.render("{}".format(HighScores[counter][1]), True, (90, 150, 240))
         games = font.render("{}".format(HighScores[counter][2]), True, (230, 220, 70))
@@ -57,10 +57,10 @@ def draw_table(HighScores):
         window.blit(highscore, (330, counter * size +200))
         window.blit(games, (550, counter * size +200))
 
-        # move to next best player
+        #move to next best player
         counter += 1
 
-    # write 'press any key to start' prompt at bottom of screen
+    #write 'press any key to start' prompt at bottom of screen
     prompt = font.render("press any key to start", True, (70, 70, 70))
     window.blit(prompt, (180,650))
 
@@ -72,7 +72,8 @@ def draw_table(HighScores):
 
 
 
-# function to place apple in valid square - FR 5.2
+
+# function to place apple in valid square
 def place_apple(grid):
     # pick random X,Y coordinate
     appleX = randint(0, gridWidth - 1)
@@ -90,13 +91,12 @@ def place_apple(grid):
 
 
 
-
-# updates the values of the game grid - FR 3.4 , FR 5.3
+# draws the game grid and score to the window
 def update_grid(head, appleX, appleY):
     # set grid to all 0s
     grid = [[0] * gridWidth for i in range(gridHeight)]
 
-    # loop through entire worm and update the grid for each segment - FR 3.4
+    # loop through entire worm and identify all the segment position in the grid where a segment is present
     segment = head
     while segment is not None:
         # get the current x y position
@@ -109,7 +109,7 @@ def update_grid(head, appleX, appleY):
         # go to the next segement in the list
         segment = segment.nextNode()
 
-    # add the apple to the grid - FR 5.3
+    # add the apple to the grid
     grid[appleX][appleY] = 2
 
     return grid
@@ -117,8 +117,6 @@ def update_grid(head, appleX, appleY):
 
 
 
-
-#draw the game grid and players score - FR 3.5
 def draw_grid(grid, score):
     # map of colours for each square type
     colour_map = {
@@ -133,6 +131,8 @@ def draw_grid(grid, score):
             # get the colour of square
             square_colour = colour_map[grid[x][y]]
             square_size = 60
+
+        # draw square to grid in the correct position and colour
 
             # positions are multiplied by size to scale up the locations from array indexes to pixels on the screen
             x_pos = x * square_size
@@ -151,114 +151,111 @@ def draw_grid(grid, score):
 
 
 
-
-
 def game_loop():
     # initialise the players score
     score = 0
 
-    # get the players username
-    username = get_username()
-    if username != '':
-        # initialsie the player
-        head = Node(gridWidth // 2, gridHeight // 2)
-        head.append()
-        head.nextNode().setPos(gridWidth // 2, gridHeight // 2 + 1)
-        direction = 'up'
+    # get a valid username from user
+    username = get_username("Please enter your username")
+    while not valid_name(username):
+        username = get_username("Username is invalid please try again...")
 
-        # initialise the grid and apple position
-        grid = [[0] * gridWidth for i in range(gridHeight)]
-        grid = update_grid(head, 0, 0)
-        appleX, appleY = place_apple(grid)
+    # initialsie the worm list
+    head = Node(gridWidth // 2, gridHeight // 2)
+    head.append()
+    head.nextNode().setPos(gridWidth // 2, gridHeight // 2 + 1)
+    direction = 'up'
 
-        # run game until player loses or quits
-        lost = False
-        while not lost:
-            window.fill((0, 0, 0))
+    # initialise the grid and apple position
+    grid = update_grid(head, 0, 0)
+    appleX, appleY = place_apple(grid)
 
-            # get inputs
-            for event in pg.event.get():
-                # end game if player quit - FR 6.3
-                if event.type == pg.QUIT:
-                    return username, score
+    # run game until player loses or quits
+    lost = False
+    while not lost:
+        window.fill((0, 0, 0))
 
-                # get arrow key input - FR 3.1 
-                if event.type == pg.KEYDOWN:
-                    # check that when a key is pressed it doesn't oppose current direction
-                    if event.key == pg.K_LEFT and direction != 'right':
-                        direction = 'left'
-                    elif event.key == pg.K_RIGHT and direction != 'left':
-                        direction = 'right'
-                    elif event.key == pg.K_UP and direction != 'down':
-                        direction = 'up'
-                    elif event.key == pg.K_DOWN and direction != 'up':
-                        direction = 'down'
-
-            # get current position of worm head
-            headx, heady = head.getPos()[0], head.getPos()[1]
-
-            # move all nodes except the head forward - FR 3.3
-            move_player(head)
-
-            # move head node depending on the current direction - FR 3.3
-            if direction == 'left':
-                head.setPos(headx - 1, heady)
-            elif direction == 'right':
-                head.setPos(headx + 1, heady)
-            elif direction == 'up':
-                head.setPos(headx, heady - 1)
-            else:
-                head.setPos(headx, heady + 1)
-
-            # check if player has eaten an apple - FR 4.1, FR 5.1
-            if detect_eat(head, appleX, appleY):
-                # replace apple - FR 5.2
-                appleX, appleY = place_apple(grid)
-                # increase players score - 4.2
-                score = score + 100
-
-            # check if player has lost the game - FR 6.1, FR 6.2
-            if check_loss(head,grid):
+        # get inputs
+        for event in pg.event.get():
+            # end game if player quit
+            if event.type == pg.QUIT:
                 return username, score
 
-            # update the values on the grid - FR 3.4, FR 5.3
-            grid = update_grid(head, appleX, appleY)
+            # get arrow key input
+            if event.type == pg.KEYDOWN:
+                # check that when a key is pressed it doesn't oppose current direction
+                if event.key == pg.K_LEFT and direction != 'right':
+                    direction = 'left'
+                elif event.key == pg.K_RIGHT and direction != 'left':
+                    direction = 'right'
+                elif event.key == pg.K_UP and direction != 'down':
+                    direction = 'up'
+                elif event.key == pg.K_DOWN and direction != 'up':
+                    direction = 'down'
 
-            # draw the grid to the screen
-            draw_grid(grid, score)
-            pg.display.update()
-            clock.tick(5)
+        # get current position of worm head
+        headx, heady = head.getPos()[0], head.getPos()[1]
+
+        # move all nodes except the head forward
+        move_player(head)
+
+        # move head node depending on the current direction
+        if direction == 'left':
+            head.setPos(headx - 1, heady)
+        elif direction == 'right':
+            head.setPos(headx + 1, heady)
+        elif direction == 'up':
+            head.setPos(headx, heady - 1)
+        else:
+            head.setPos(headx, heady + 1)
+
+        # check if player has eaten an apple
+        if detect_eat(head, appleX, appleY):
+            # replace apple
+            appleX, appleY = place_apple(grid)
+            # increase players score
+            score = score + 100
+
+        # check if player has lost the game
+        if check_loss(head,grid):
+            return username, score
+
+        # update the values on the grid
+        grid = update_grid(head, appleX, appleY)
+
+        # draw the grid to the screen
+        draw_grid(grid, score)
+        pg.display.update()
+        clock.tick(5)
 
     return username, score
 
 
-#reoeat until game closed
+# until the window is closed
 closed = False
 while not closed:
-    #get table from database and draw it - FR 1.1 -> 1.5
+    # get sorted high score table
     table = get_table()
+    # display sorted high score table
     draw_table(table)
-    
+
+    # wait until a key press or the window is closed
     keypressed = False
     while not keypressed:
         for event in pg.event.get():
-            # end game if quit - FR 1.6
+            # close game if user quits
             if event.type == pg.QUIT:
                 closed = True
                 keypressed=True
                 pg.quit()
-            # end loop is key pressed
+
             if event.type == pg.KEYDOWN:
                 keypressed = True
 
+    # if the window wasn't closed
     if not closed:
-        # get the game details by starting a new game
+        # play the game and get score and username
         username, score = game_loop()
 
-        # check if username was not null
-        if username != '':
-            # insert game details into table - Fr 2.3
-            add_game(username, score)
-        else:
-            # if no username was entered 
-            closed = True
+        # INSERT name and score into database
+        add_game(username, score)
