ff378de118940ead27e4694e2ebb8340bfc69bd0
ArchieUrquhart <128182020+ArchieUrquhart@users.noreply.github.com>
2025-03-04 13:07:43 +0000

    Update UI.py

--- UI.py
+++ UI.py
@@ -2,7 +2,7 @@ import re
 from tkinter import *
 
 
-#function to validate username - FR 2.2
+#function to validate username
 def valid_name(username):
     #check the length of username and regex to check if there any special characters
     if len(username) > 15 or len(username) < 4 or re.search("\W",username):
@@ -10,28 +10,28 @@ def valid_name(username):
     else:
         return True
 
-#return username when button pressed
-def get_input(root, inputtxt, label):
+#retrives the username from the text box
+def get_input(root, inputtxt):
     global username
     input = inputtxt.get("1.0", "end-1c")
-    if valid_name(input):
-        root.destroy()
-        username = input
-    else:
-        label.configure(text="Please re-enter your username")
+    root.destroy()
+    username = input
 
-# FR 2.1
-def get_username():
+#gets username from user with a variable prompt
+def get_username(message):
     global username
     username = ""
 
+    #sets up window
     root = Tk()
     root.title("Worm")
 
-    prompt = Label(text="Please enter your username")
+    #creates all window features
+    prompt = Label(text=message)
     inputtxt = Text(root, height=1,width=20,padx=20,pady=10)
-    button = Button(root, height=2,width=8,text="Play",command=lambda: get_input(root,inputtxt,prompt))
+    button = Button(root, height=2,width=8,text="Play",command=lambda: get_input(root,inputtxt))
 
+    #positioning of features
     prompt.grid(row=0,column=0)
     inputtxt.grid(row=1, column=0)
     button.grid(row=1, column=1)
@@ -40,6 +40,3 @@ def get_username():
     mainloop()
 
     return username
-
-    return username
-
